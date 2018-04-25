99dde1c63c244d13f76c7ba5ed02ad520509f1c9
Jody LeCompte <jody@jodylecompte.com>
2018-04-25 15:14:54 -0500

    Initial Rouing
    Routing has been setup for just the few core modules I know there will
    be now. It will likely need to be flushed out more, put into it own
    file, and test coverage added once user options and parameters are
    added.
    
    Skeletons have been generated for all main components as well, each
    in their own directory to eventually house their children as well

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,16 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { CampaignsComponent } from './campaigns/campaigns.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { UsersComponent } from './users/users.component';
+import { SettingsComponent } from './settings/settings.component';
 
+const appRoutes: Routes = [
+  { path: '', pathMatch: 'full', component: DashboardComponent},
+  { path: 'dashboard', component: DashboardComponent},
+  { path: 'campaigns', component: CampaignsComponent },
+  { path: 'users', component: UsersComponent},
+  { path: 'settings', component: SettingsComponent },
+  { path: '**', component: CampaignsComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CampaignsComponent,
+    DashboardComponent,
+    UsersComponent,
+    SettingsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
